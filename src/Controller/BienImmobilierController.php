14fcf77457b9b5db5bccce5cf4f9e2cc83061dbd
Terry De la soudiere <terry66000@gmail.com>
2019-06-23 17:19:21 +0200

    ajout controller

---
+++ src/Controller/BienImmobilierController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry
+ * Date: 23/06/2019
+ * Time: 17:14
+ */
+
+namespace App\Controller;
+
+
+class BienImmobilierController
+{
+
+}
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Terry
+ * Date: 23/06/2019
+ * Time: 17:02
+ */
+
+namespace App\Controller;
+
+
+class HomeController
+{
+
+}
